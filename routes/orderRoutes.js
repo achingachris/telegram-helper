90205e3dcf49c68767b08aad6ac29ad2b222b509
chrisdevcode <achinga.chris@gmail.com>
2022-07-10 22:09:53 +0300

    order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import {
-  addOrderItems,
-  getOrderById,
-  updateOrderToPaid,
-  updateOrderToDelivered,
-  getMyOrders,
-  getOrders,
-} from '../controllers/orderController.js'
+
+// controllers
+import addOrderItems from '../controllers/orders/addOrderItems.js'
+import getOrderById from '../controllers/orders/getOrderById.js'
+import updateOrderToPaid from '../controllers/orders/updateOrderToPaid.js'
+import updateOrderToDelivered from '../controllers/orders/updateOrderToDelivered.js'
+import getMyOrders from '../controllers/orders/getMyOrders.js'
+import getOrders from '../controllers/orders/getOrders.js'
+
+
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
