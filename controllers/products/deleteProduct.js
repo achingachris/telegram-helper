f98c49e0d83a143e4c4a5a4136315263b28f682a
chrisdevcode <achinga.chris@gmail.com>
2022-07-10 21:56:25 +0300

    restructure product controllers

---
+++ controllers/products/deleteProduct.js
@@ -0,0 +1,19 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../../models/productModel.js'
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export default deleteProduct
